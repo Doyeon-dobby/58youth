b98b5f3cd5bc676c44a84a6b0277ec53b9d74f1b
sjwoo7 <116350801+sjwoo7@users.noreply.github.com>
2023-08-18 03:33:28 +0900

    Add files via upload

---
+++ src/components/2_FindFriend/2_4/Element.jsx
@@ -0,0 +1,188 @@
+import React from "react";
+import { AndroidStatusBar } from "../../components/AndroidStatusBar";
+import { GestureThree } from "../../components/GestureThree";
+import "./style.css";
+
+export const Element = () => {
+  return (
+    <div className="element">
+      <div className="div-2">
+        <div className="overlap">
+          <div className="view">
+            <div className="frame">
+              <div className="text-wrapper-2">친구</div>
+            </div>
+          </div>
+          <div className="view-2" />
+          <div className="overlap-group-wrapper">
+            <div className="overlap-group">
+              <div className="text-wrapper-3">내 닉네임</div>
+              <img className="img" alt="Rectangle" src="/img/rectangle-68-5.png" />
+              <div className="frame-wrapper">
+                <div className="frame">
+                  <div className="text-wrapper-2">프로필 보기</div>
+                </div>
+              </div>
+              <div className="text-wrapper-4">상태메세지</div>
+            </div>
+          </div>
+          <div className="div-wrapper">
+            <div className="frame-2">
+              <div className="text-wrapper-5">친구 목록</div>
+            </div>
+          </div>
+          <div className="view-3">
+            <div className="frame-3">
+              <div className="text-wrapper-6">환경설정</div>
+            </div>
+            <img className="settings" alt="Settings" src="/img/settings-1.svg" />
+          </div>
+          <div className="text-wrapper-7">광고</div>
+          <div className="div-3" />
+          <div className="view-4">
+            <div className="overlap-group-wrapper-2">
+              <div className="overlap-group">
+                <div className="text-wrapper-8">닉네임</div>
+                <div className="text-wrapper-9">,XX세</div>
+                <img className="rectangle-2" alt="Rectangle" src="/img/rectangle-68.png" />
+                <div className="frame-wrapper">
+                  <div className="frame">
+                    <div className="text-wrapper-2">프로필 보기</div>
+                  </div>
+                </div>
+                <div className="text-wrapper-10">상태메세지</div>
+              </div>
+            </div>
+            <div className="overlap-wrapper">
+              <div className="overlap-group">
+                <div className="text-wrapper-8">닉네임</div>
+                <div className="text-wrapper-9">,XX세</div>
+                <img className="rectangle-2" alt="Rectangle" src="/img/rectangle-68.png" />
+                <div className="frame-wrapper">
+                  <div className="frame">
+                    <div className="text-wrapper-2">프로필 보기</div>
+                  </div>
+                </div>
+                <div className="text-wrapper-10">상태메세지</div>
+              </div>
+            </div>
+            <div className="overlap-wrapper-2">
+              <div className="overlap-group">
+                <div className="text-wrapper-8">닉네임</div>
+                <div className="text-wrapper-9">,XX세</div>
+                <img className="rectangle-2" alt="Rectangle" src="/img/rectangle-68.png" />
+                <div className="frame-wrapper">
+                  <div className="frame">
+                    <div className="text-wrapper-2">프로필 보기</div>
+                  </div>
+                </div>
+                <div className="text-wrapper-10">상태메세지</div>
+              </div>
+            </div>
+            <div className="overlap-wrapper-3">
+              <div className="overlap-group">
+                <div className="text-wrapper-8">닉네임</div>
+                <div className="text-wrapper-9">,XX세</div>
+                <img className="rectangle-2" alt="Rectangle" src="/img/rectangle-68.png" />
+                <div className="frame-wrapper">
+                  <div className="frame">
+                    <div className="text-wrapper-2">프로필 보기</div>
+                  </div>
+                </div>
+                <div className="text-wrapper-10">상태메세지</div>
+              </div>
+            </div>
+            <div className="overlap-wrapper-4">
+              <div className="overlap-group">
+                <div className="text-wrapper-8">닉네임</div>
+                <div className="text-wrapper-9">,XX세</div>
+                <img className="rectangle-2" alt="Rectangle" src="/img/rectangle-68.png" />
+                <div className="frame-wrapper">
+                  <div className="frame">
+                    <div className="text-wrapper-2">프로필 보기</div>
+                  </div>
+                </div>
+                <div className="text-wrapper-10">상태메세지</div>
+              </div>
+            </div>
+            <div className="div-4" />
+            <div className="div-5" />
+            <div className="div-6" />
+            <div className="div-7" />
+          </div>
+          <div className="group-wrapper">
+            <div className="group">
+              <div className="view-5">
+                <div className="overlap-group-2">
+                  <div className="text-wrapper-11">닉네임 님을 차단했습니다</div>
+                  <div className="view-6">
+                    <div className="text-wrapper-12">차단친구는</div>
+                    <div className="text-wrapper-13">관리할 수 있습니다.</div>
+                    <div className="group-2">
+                      <div className="text-wrapper-14">환경설정 에서</div>
+                      <img className="settings-2" alt="Settings" src="/img/settings.svg" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="view-7">
+          <div className="overlap-2">
+            <div className="union-wrapper">
+              <img className="union-2" alt="Union" src="/img/union-3.svg" />
+            </div>
+            <p className="p">
+              <span className="span">오팔</span>
+              <span className="text-wrapper-15">청춘</span>
+            </p>
+            <div className="view-8">
+              <div className="group-3">
+                <div className="text-wrapper-16">가</div>
+                <div className="text-wrapper-17">가</div>
+                <div className="text-wrapper-18">가</div>
+              </div>
+            </div>
+            <img className="arrow-back-ios" alt="Arrow back ios" src="/img/arrow-back-ios.svg" />
+          </div>
+          <div className="div-8" />
+          <div className="div-9" />
+        </div>
+        <div className="div-10">
+          <div className="overlap-3">
+            <div className="frame-4">
+              <div className="view-9">
+                <img className="img-2" alt="Search" src="/img/search.svg" />
+                <div className="text-wrapper-19">친구찾기</div>
+              </div>
+              <div className="view-9">
+                <div className="text-wrapper-19">친구목록</div>
+                <img className="img-2" alt="Person" src="/img/person.svg" />
+              </div>
+              <div className="view-10">
+                <div className="text-wrapper-20">오늘의 인연</div>
+                <img className="auto-awesome" alt="Auto awesome" src="/img/auto-awesome.svg" />
+              </div>
+              <div className="div-11">
+                <div className="text-wrapper-21">1:1 대화방</div>
+                <div className="group-wrapper-2">
+                  <div className="group-4">
+                    <div className="overlap-group-3">
+                      <img className="union-3" alt="Union" src="/img/union-2.svg" />
+                      <img className="subtract" alt="Subtract" src="/img/subtract.svg" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <img className="image" alt="Image" src="/img/2x.png" />
+            </div>
+          </div>
+          <div className="rectangle-3" />
+        </div>
+        <GestureThree className="gesture-three-buttons" />
+        <AndroidStatusBar className="android-status-bar-instance" darkMode={false} />
+      </div>
+    </div>
+  );
+};
